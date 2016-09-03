23173a0eb84f361483a845b3f20ad85373f8e2fb
ishantvivek <ishantvivek1311@gmail.com>
2016-09-03 11:32:39 +0530

    Partially revert "kyleprods : Few updates"
    This reverts commit e2ecf3deeb613bb98332196c4b19f36d58a41db2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ BOARD_FLASH_BLOCK_SIZE                      := 262144
 BOARD_HAVE_BLUETOOTH                        := true
 BOARD_HAVE_BLUETOOTH_BCM                    := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/kyleprods/bluetooth
-BOARD_CUSTOM_BT_CONFIG                 := device/samsung/kyleprods/bluetooth/libbt_vndcfg.txt
+BOARD_CUSTOM_BT_CONFIG                      := device/samsung/kyleprods/bluetooth/libbt_vndcfg.txt
 
 # Connectivity - Wi-Fi
 BOARD_HAVE_SAMSUNG_WIFI                     := true
