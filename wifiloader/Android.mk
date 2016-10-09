d776f9c072ca6a602116a1ef480923505c8c8f49
ishantvivek <ishantvivek1311@gmail.com>
2016-10-09 09:40:49 +0000

    hawaii: Add wifiloader and macloader
    * This was previously in hardware/samsung repo,move to device tree
    
    Signed-off-by: ishantvivek <ishantvivek1311@gmail.com>

---
+++ wifiloader/Android.mk
@@ -0,0 +1,15 @@
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    wifiloader.c
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog libutils
+
+LOCAL_MODULE := wifiloader
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
