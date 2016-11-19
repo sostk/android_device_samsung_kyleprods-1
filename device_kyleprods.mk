f76c6678353cf9304c2c4c741910d7c99a050623
ishantvivek <ishantvivek1311@gmail.com>
2016-11-19 17:24:08 +0000

    kyleproxx: Implement dt2w support
    * Use native support of AOSP
    * Update device hardware keys
    
    Signed-off-by: ishantvivek <ishantvivek1311@gmail.com>

--- device_kyleprods.mk
+++ device_kyleprods.mk
@@ -95,6 +95,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Powerhal
+PRODUCT_PACKAGES += \
+    power.hawaii
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
