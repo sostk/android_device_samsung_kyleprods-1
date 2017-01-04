sostk/android_device_samsung_kylepro-1

Welcome to LineageOS
* RIP CM :'(

BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_TAGS_OFFSET                    := 0x00000100
 ifeq ($(BUILD_TWRP),true)
     TARGET_KERNEL_CONFIG                    := bcm21664_hawaii_ss_kyleproxx_rev00_recovery_defconfig
 else
-    TARGET_KERNEL_CONFIG                    := bcm21664_hawaii_ss_kylepro_rev00_cyanogenmod_defconfig
+    TARGET_KERNEL_CONFIG                    := bcm21664_hawaii_ss_kylepro_rev00_lineage_defconfig
 endif
 TARGET_KERNEL_SOURCE                        := kernel/samsung/kyleproxx
 

lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := cm_kylepro
+PRODUCT_NAME := lineage_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-    add_lunch_combo cm_kylepro-${i}
+    add_lunch_combo lineage_kylepro-${i}
 done

sostk/android_device_samsung_kyleprods-1

BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_TAGS_OFFSET                    := 0x00000100
 ifeq ($(BUILD_TWRP),true)
     TARGET_KERNEL_CONFIG                    := bcm21664_hawaii_ss_kyleproxx_rev00_recovery_defconfig
 else
-    TARGET_KERNEL_CONFIG                    := bcm21664_hawaii_ss_kyleprods_rev00_cyanogenmod_defconfig
+    TARGET_KERNEL_CONFIG                    := bcm21664_hawaii_ss_kyleprods_rev00_lineage_defconfig
 endif
 TARGET_KERNEL_SOURCE                        := kernel/samsung/kyleproxx
 

lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleprods
-PRODUCT_NAME := cm_kyleprods
+PRODUCT_NAME := lineage_kyleprods
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7582

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-    add_lunch_combo cm_kyleprods-${i}
+    add_lunch_combo lineage_kyleprods-${i}
 done