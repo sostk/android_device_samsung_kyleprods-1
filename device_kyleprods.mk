3ff0a582cfa79e9598ada13715f09db8aa4902ae
DodoGTA GT <vwestlife20@gmail.com>
2017-01-29 09:52:09 +0200

    kyleprods: Fix derps

--- device_kyleprods.mk
+++ device_kyleprods.mk
@@ -57,12 +57,12 @@ PRODUCT_PACKAGES += \
 # Stagefright FFmpeg plugin/FFmpeg config
 PRODUCT_PACKAGES += \
     libffmpeg_extractor \
-	libffmpeg_omx \
-	media_codecs_ffmpeg.xml
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.sf.omx-plugin=libffmpeg_omx.so \
-	media.sf.extractor-plugin=libffmpeg_extractor.so
+    media.sf.extractor-plugin=libffmpeg_extractor.so
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
